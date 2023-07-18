e8bed6ec1cbcec35c15b21e729050b43eb3744fd
Vitor Moura <663103+vitormoura@users.noreply.github.com>
2023-07-18 10:28:11 -0400

    ajoute support à validation de certificats

--- src/main/java/local/projects/myhttpclient/MyHttpClient.java
+++ src/main/java/local/projects/myhttpclient/MyHttpClient.java
@@ -4,11 +4,21 @@
 package local.projects.myhttpclient;
 
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.security.KeyManagementException;
+import java.security.KeyStore;
 import java.security.KeyStoreException;
 import java.security.NoSuchAlgorithmException;
+import java.security.cert.CertPath;
+import java.security.cert.CertPathValidator;
+import java.security.cert.CertPathValidatorResult;
 import java.security.cert.CertificateException;
+import java.security.cert.CertificateFactory;
+import java.security.cert.PKIXParameters;
+import java.security.cert.PKIXRevocationChecker;
+import java.security.cert.X509Certificate;
+import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.net.ssl.SSLContext;
@@ -37,29 +47,135 @@ import org.apache.http.util.EntityUtils;
  */
 public class MyHttpClient {
 
+    static final String OPT_CMD = "cmd";
+    static final String OPT_CERTCHAIN_FILEPATH = "certchain_filepath";
+    static final String OPT_CERT_FILEPATH = "cert_filepath";
+    static final String OPT_KS_FILEPATH = "keystore_filepath";
+    static final String OPT_KS_PASSWORD = "keystore_password";
+    static final String OPT_PROXY_HOST = "proxy_host";
+    static final String OPT_PROXY_PORT = "proxy_port";
+    static final Logger logger = Logger.getLogger(MyHttpClient.class.getName());
+
     public static void main(String[] args) throws ParseException {
 
-        Logger logger = Logger.getLogger(MyHttpClient.class.getName());
-        SSLContext sslcontext;
         Options options;
-
-        final String OPT_KS_FILEPATH = "keystore_filepath";
-        final String OPT_KS_PASSWORD = "keystore_password";
-        final String OPT_PROXY_HOST = "proxy_host";
-        final String OPT_PROXY_PORT = "proxy_port";
 
         // Command arg options
         options = new Options();
-        options.addRequiredOption("u", "url", true, "test url")
+        options.addRequiredOption("c", "cmd", true, "command to execute")
+                .addOption("u", "url", true, "test url")
+                .addOption(OPT_CERT_FILEPATH, true, "path to certificate")
+                .addOption(OPT_CERTCHAIN_FILEPATH, true, "path to certificate ca chain")
                 .addOption(OPT_KS_FILEPATH, true, "path to certificate keystore")
                 .addOption(OPT_KS_PASSWORD, true, "keystore password")
                 .addOption(OPT_PROXY_HOST, true, "proxy host")
                 .addOption(OPT_PROXY_PORT, true, "proxy port (default 8080)");
 
+        CommandLineParser parser = new DefaultParser();
+        CommandLine cmd = parser.parse(options, args);
+        String cmdToExec = cmd.getOptionValue(OPT_CMD).toLowerCase();
+
+        switch (cmdToExec) {
+            case "http:get":
+                execHttpGetRequest(cmd);
+                break;
+
+            case "cert:check":
+                execCertCheck(cmd);
+                break;
+
+            default:
+                System.err.println("ERR invalid command: " + cmdToExec);
+        }
+
+    }
+
+    public static void execCertCheck(CommandLine cmd) {
+
+        // https://docs.oracle.com/javase/8/docs/technotes/guides/security/certpath/CertPathProgGuide.html
+        // CertPath
+        // CertificateFactory: Peut créer des CertPaths depuis des listes de certificats ou des fichiers (generateCertPath)
+        // CertPathParameters (PKIXParameters)
+        // CertPathValidator
+        // CertPathValidatorResult
+        // CertPathBuilder
+        // CertPathBuilderResult
+        // CertStore (CertSelector, CRLSelector, X509CertSelector,X509CertSelector)
+        // X509CertSelector
+        // TrustAnchor
+        // PKIXBuilderParameters 
+        // PKIXCertPathChecker
+        // PKIXRevocationChecker
+        //
+        // Valide arguments obligatoires
+        //
+        String[] requiredArgs = new String[]{OPT_KS_FILEPATH, OPT_CERT_FILEPATH};
+
+        for (String arg : requiredArgs) {
+            if (!cmd.hasOption(arg)) {
+                System.err.println("required argument: " + arg);
+                System.exit(1);
+            }
+        }
+
+        // 
+        // Arguments
+        //
+        String keyStoreFilePath = cmd.getOptionValue(OPT_KS_FILEPATH);
+        String keyStorePassword = cmd.getOptionValue(OPT_KS_PASSWORD);
+        String targetCertFile = cmd.getOptionValue(OPT_CERT_FILEPATH);
+
+        // 
+        // Variables
+        //
+        KeyStore keyStore;
+        CertificateFactory certFactory;
+        X509Certificate targetCert;
+
         try {
 
-            CommandLineParser parser = new DefaultParser();
-            CommandLine cmd = parser.parse(options, args);
+            keyStore = getKeyStore(keyStoreFilePath, keyStorePassword);
+            certFactory = CertificateFactory.getInstance("X.509");
+
+            //
+            // Ouvre fichier certificat à valider
+            // 
+            try (FileInputStream in = new FileInputStream(targetCertFile)) {
+                targetCert = (X509Certificate) certFactory.generateCertificate(in);
+            } catch (IOException ex) {
+                logger.log(Level.SEVERE, "erreur s'est produite lors de la recharge du certificat", ex);
+                System.exit(1);
+                return;
+            }
+
+            logger.log(Level.INFO, "certificat charg\u00e9: {0} ({1})", new String[]{targetCertFile, targetCert.getSubjectDN().getName()});
+
+            //
+            // Valide certificat
+            //
+            CertPathValidator pathValidator = CertPathValidator.getInstance(CertPathValidator.getDefaultType());
+            CertPath defaultCertPath = certFactory.generateCertPath(List.of(targetCert));
+
+            PKIXRevocationChecker certPathChecker = (PKIXRevocationChecker) pathValidator.getRevocationChecker();
+
+            // Config validation
+            PKIXParameters pathValidatorParams = new PKIXParameters(keyStore);
+
+            pathValidatorParams.addCertPathChecker(certPathChecker);
+            pathValidatorParams.setRevocationEnabled(true);
+            
+            CertPathValidatorResult pathValidatorResult = pathValidator.validate(defaultCertPath, pathValidatorParams);
+
+            // resultat
+            System.out.println(pathValidatorResult.toString());
+
+        } catch (Exception ex) {
+            logger.log(Level.SEVERE, "une erreur s'est produite lors de la validation du certificat", ex);
+        }
+    }
+
+    public static void execHttpGetRequest(CommandLine cmd) {
+        try {
 
             SSLContextBuilder sslContextBuilder = SSLContexts.custom();
 
@@ -77,11 +193,12 @@ public class MyHttpClient {
                         cmd.hasOption(OPT_KS_PASSWORD) ? cmd.getOptionValue(OPT_KS_PASSWORD).toCharArray() : null,
                         new TrustSelfSignedStrategy()
                 );
+
             }
 
+            SSLContext sslcontext;
             sslcontext = sslContextBuilder.build();
 
-            // Allow TLSv1 protocol only
             SSLConnectionSocketFactory sslsf = new SSLConnectionSocketFactory(
                     sslcontext,
                     null,
@@ -132,7 +249,24 @@ public class MyHttpClient {
         } catch (IOException | NoSuchAlgorithmException | KeyStoreException | CertificateException | KeyManagementException ex) {
             logger.log(Level.SEVERE, null, ex);
         }
+    }
 
+    private static KeyStore getKeyStore(String keyStoreFilePath, String keyStorePassword) throws Exception {
+        KeyStore keyStore;
+        File certStoreFile = new File(keyStoreFilePath);
+
+        try {
+
+            char[] keystorePasswordChars = keyStorePassword != null ? keyStorePassword.toCharArray() : null;
+            keyStore = KeyStore.getInstance(certStoreFile, keystorePasswordChars);
+
+            logger.log(Level.INFO, "keystore charg\u00e9: {0}", keyStoreFilePath);
+
+            return keyStore;
+
+        } catch (IOException | KeyStoreException | NoSuchAlgorithmException | CertificateException ex) {
+            throw ex;
+        }
     }
 
 }
